0649d419fb1fc23d0fdba456ae507e7fe0e15e97
Yavor Keskinev <yavor_keskinev@abv.bg>
2019-01-07 23:55:06 +0200

    Update projectile.js

--- server/client/scripts/projectile.js
+++ server/client/scripts/projectile.js
@@ -1,14 +1,14 @@
 class Projectile {
 
-    constructor(startX, startY, endPt, index) {
+    constructor(startX, startY, endPtX, endPtY, index) {
         this.speed = 120;
         this.radius = 5;
         this.originPointX = startX;
         this.originPointY = startY;
         this.curX = startX;
         this.curY = startY;
-        this.endX = endPt.x;
-        this.endY = endPt.y;
+        this.endX = endPtX;
+        this.endY = endPtY;
         this.distanceX = this.endX - this.originPointX;
         this.distanceY = this.endY - this.originPointY;
         this.targetIndex = index;
@@ -22,8 +22,8 @@ class Projectile {
     }
     
     move() {
-        this.curX += this.distanceX / 30;
-        this.curY += this.distanceY / 30;
+        this.curX += this.distanceX / 15;
+        this.curY += this.distanceY / 15;
         if(Math.abs(this.curX - this.originPointX) > Math.abs(this.distanceX) && Math.abs(this.curY - this.originPointY) > Math.abs(this.distanceY)) {
             return false;
         }
