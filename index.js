16946b946eb100c62016d26bdcafebfc4fce0239
Yavor Keskinev <yavor_keskinev@abv.bg>
2019-01-08 16:08:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = require('express')();
 var http = require('http').Server(app);
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('client'))
 
@@ -8,6 +9,6 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/client/index.html');
 });
 
-http.listen(3000, function(){
+http.listen(PORT, function(){
     console.log('listening on *:3000');
 });
